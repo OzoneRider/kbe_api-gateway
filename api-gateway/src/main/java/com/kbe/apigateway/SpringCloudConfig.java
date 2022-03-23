05f101ffcb3a7957a893450f1d4a0c3cd1166fcf
JoJo <Georgios.Padopoulos@outlook.de>
2022-03-23 19:06:32 +0100

    added cloud gateway dependency and added application route

---
+++ api-gateway/src/main/java/com/kbe/apigateway/SpringCloudConfig.java
@@ -0,0 +1,19 @@
+package com.kbe.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route("path_route", r -> r.path("/products/**").and().method("GET")
+                .uri("http://localhost:4444"))
+                .build();
+    }
+}
