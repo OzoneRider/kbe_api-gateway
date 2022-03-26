3e3c2d5215cc9140e07846841a8e6bf7e42f4535
JoJo <Georgios.Padopoulos@outlook.de>
2022-03-26 16:31:05 +0100

    enable cross origin

--- api-gateway/src/main/java/com/kbe/apigateway/configuration/SpringCloudConfig.java
+++ api-gateway/src/main/java/com/kbe/apigateway/configuration/SpringCloudConfig.java
@@ -1,4 +1,4 @@
-package com.kbe.apigateway;
+package com.kbe.apigateway.configuration;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
